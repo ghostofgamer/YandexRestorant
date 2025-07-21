c0e79b93c64e0810ea184797127de1e901d4e849
ghostofgamer <105151923+ghostofgamer@users.noreply.github.com>
2025-07-21 18:40:17 +0400

    StartInapPriceCurrentRegion

--- Assets/Scripts/UI/Screens/AdsScreens/StarterPackScreen.cs
+++ Assets/Scripts/UI/Screens/AdsScreens/StarterPackScreen.cs
@@ -1,9 +1,20 @@
+using MirraGames.SDK;
+using MirraGames.SDK.Common;
+using TMPro;
 using UnityEngine;
 
 namespace UI.Screens.AdsScreens
 {
     public class StarterPackScreen : AbstractScreen
     {
+        [SerializeField] private TMP_Text _valuteText;
 
+        public void Start()
+        {
+            ProductData productData = MirraSDK.Payments.GetProductData("StarterPack");
+            _valuteText.text = $"{productData.Currency} {productData.PriceInteger}";
+            Debug.Log(productData.PriceInteger);
+                Debug.Log(productData.Currency);
+        }
     }
 }
